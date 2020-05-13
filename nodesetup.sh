df767bd54454eadd59bc7dc40bcbe57cf8a95006
jyoung2119 <46932502+jyoung2119@users.noreply.github.com>
2020-05-13 14:08:01 -0500

    Create nodesetup.sh

---
+++ nodesetup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+sudo apt-get install -y batctl
+
+sudo cat > ~/start-batman-adv.sh <<- "EOF"
+#!/bin/bash
+# batman-adv interface to use
+sudo batctl if add wlan0
+sudo ifconfig bat0 mtu 1468
+
+# Tell batman-adv this is a gateway client
+sudo batctl gw_mode client
+
+# Activates batman-adv interfaces
+sudo ifconfig wlan0 up
+sudo ifconfig bat0 up
+EOF
+
+sudo chmod +x ~/start-batman-adv.sh
+
+sudo cat > /etc/network/interfaces.d/wlan0 <<- "EOF"
+auto wlan0
+iface wlan0 inet manual
+    wireless-channel 1
+    wireless-essid call-code-mesh
+    wireless-mode ad-hoc
+EOF
+
+echo 'batman-adv' | sudo tee --append /etc/modules
+
+echo 'denyinterfaces wlan0' | sudo tee --append /etc/dhcpcd.conf
+
+sudo sed '18 a /home/pi/start-batman-adv.sh &' /etc/rc.local
+
+echo 'NODE SETUP COMPLETE'
+echo 'IF PI STAYING AS NODE PLEASE SHUTDOWN'
