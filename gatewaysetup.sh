ea7baecd94db4e3639d632dc0e83f2355763a333
jyoung2119 <46932502+jyoung2119@users.noreply.github.com>
2020-05-13 15:58:29 -0500

    Update gatewaysetup.sh

--- gatewaysetup.sh
+++ gatewaysetup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install -y dnsmasq
+
 sudo sed -i '$ a interface=bat0\ndhcp-range=192.168.199.2,192.168.199.99,255.255.255.0,12h' /etc/dnsmasq.conf
 # echo $'interface=bat0\ndhcp-range=192.168.199.2,192.168.199.99,255.255.255.0,12h' | sudo tee --append /etc/dnsmasq.conf
 
