d7034ce30711a4744a20010c63091f47af959b95
jyoung2119 <46932502+jyoung2119@users.noreply.github.com>
2020-05-13 15:50:39 -0500

    Update gatewaysetup.sh

--- gatewaysetup.sh
+++ gatewaysetup.sh
@@ -5,4 +5,25 @@ sudo sed -i '$ a interface=bat0\ndhcp-range=192.168.199.2,192.168.199.99,255.255
 
 sudo rm start-batman-adv.sh
 
-sudo cat > ~/start-batman-adv.sh <<-
+sudo cat > ~/start-batman-adv.sh <<- "EOF"
+#!/bin/bash
+# batman-adv interface to use
+sudo batctl if add wlan0
+sudo ifconfig bat0 mtu 1468
+
+# Tell batman-adv this is an internet gateway
+sudo batctl gw_mode server
+
+# Enable port forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o bat0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i bat0 -o eth0 -j ACCEPT
+
+# Activates batman-adv interfaces
+sudo ifconfig wlan0 up
+sudo ifconfig bat0 up
+sudo ifconfig bat0 192.168.199.1/24
+EOF
+
+echo 'GATEWAY CREATED...SHUTDOWN PLEASE'
