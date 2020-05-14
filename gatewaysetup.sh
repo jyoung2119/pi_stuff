b1f59f90d21336f2b56e28c0e4b760e089a6e523
jyoung2119 <46932502+jyoung2119@users.noreply.github.com>
2020-05-14 10:27:07 -0500

    Update gatewaysetup.sh

--- gatewaysetup.sh
+++ gatewaysetup.sh
@@ -2,11 +2,14 @@
 
 sudo apt-get install -y dnsmasq
 
+# Add lines at end of dnsmasq.conf
 sudo sed -i '$ a interface=bat0\ndhcp-range=192.168.199.2,192.168.199.99,255.255.255.0,12h' /etc/dnsmasq.conf
 # echo $'interface=bat0\ndhcp-range=192.168.199.2,192.168.199.99,255.255.255.0,12h' | sudo tee --append /etc/dnsmasq.conf
 
-sudo rm start-batman-adv.sh
+# Remove script created by node setup
+# sudo rm start-batman-adv.sh
 
+# Overwrite existing node script
 sudo cat > ~/start-batman-adv.sh <<- "EOF"
 #!/bin/bash
 # batman-adv interface to use
