b7723bbf4daf02cb6e2deaba4217092d7517a68a
jyoung2119 <46932502+jyoung2119@users.noreply.github.com>
2020-05-14 14:29:46 -0500

    Create wifigateway.sh

---
+++ mesh_stuff/wifigateway.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+read -p "Enter Country Code: " country
+echo "Country Code: $country"
+read -p "Enter Network Name: " name
+echo "Desired Network Name: $name"
+read -s -p "Enter Network Password: " pword
+
+sudo cat > /etc/wpa_supplicant/wpa_supplicant.conf. <<- "EOF"
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=$country
+network={
+    ssid="'$name'"
+    scan_ssid=1
+    psk="'$pword'"
+}
+EOF
+
+sudo cat > ~/start-batman-adv.sh <<- "EOF"
+#!/bin/bash
+# batman-adv interface to use
+sudo batctl if add wlan0
+sudo ifconfig bat0 mtu 1468
+
+# Tell batman-adv this is an internet gateway
+sudo batctl gw_mode server
+
+# Enable port forwarding between eth0 and bat0
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE
+sudo iptables -A FORWARD -i wlan1 -o bat0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i bat0 -o wlan1 -j ACCEPT
+
+# Activates the interfaces for batman-adv
+sudo ifconfig wlan0 up
+sudo ifconfig bat0 up # bat0 is created via the first command
+sudo ifconfig bat0 192.168.199.1/24
+EOF
+
+echo 'WIFI GATEWAY SETUP COMPLETE...PLEASE SHUTDOWN'
