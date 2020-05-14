ffa96dff81fb723f3c6037a3eb8d55d46eb4d528
jyoung2119 <46932502+jyoung2119@users.noreply.github.com>
2020-05-14 15:06:10 -0500

    messing with variables

--- mesh_stuff/wifigateway.sh
+++ mesh_stuff/wifigateway.sh
@@ -4,23 +4,24 @@ read -p "Enter Country Code: " country
 echo "Country Code: $country"
 read -p "Enter Network Name: " name
 echo "Desired Network Name: $name"
-name=\"${name}\"
 read -p "Enter Network Password: " pword
-echo "Password: $pword"
-pword=\"${pword}\"
 
 sudo chmod 777 /etc/wpa_supplicant/wpa_supplicant.conf
 
 sudo cat > /etc/wpa_supplicant/wpa_supplicant.conf <<- "EOF"
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
-country=$country
+country=GB
 network={
-    ssid=$name
+    ssid="network name"
     scan_ssid=1
-    psk=$pword
+    psk="password"
 }
 EOF
+
+sed -i -e"s/^country=.*/country=$country/" /etc/wpa_supplicant/wpa_supplicant.conf
+sed -i -e"s/^ssid=.*/ssid=$name/" /etc/wpa_supplicant/wpa_supplicant.conf
+sed -i -e"s/^psk=.*/psk=$pword/" /etc/wpa_supplicant/wpa_supplicant.conf
 
 sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf
 
