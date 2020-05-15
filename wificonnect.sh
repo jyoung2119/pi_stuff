a81f3905f5d9af02b5a5e1abc85e50f22928dcd7
jyoung2119 <46932502+jyoung2119@users.noreply.github.com>
2020-05-15 11:35:11 -0500

    change file manipulation method

--- wificonnect.sh
+++ wificonnect.sh
@@ -6,25 +6,33 @@ read -p "Enter Network Name: " name
 echo "Desired Network Name: $name"
 read -p "Enter Network Password: " pword
 
-sudo chmod 777 /etc/wpa_supplicant/wpa_supplicant.conf
+#sudo chmod 777 /etc/wpa_supplicant/wpa_supplicant.conf
 
-sudo cat > /etc/wpa_supplicant/wpa_supplicant.conf <<- "EOF"
-ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-update_config=1
-country=GB
-network={
-    ssid="network name"
-    scan_ssid=1
-    psk="password"
-}
-EOF
+#sudo cat > /etc/wpa_supplicant/wpa_supplicant.conf <<- "EOF"
+#ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+#update_config=1
+#country=GB
+#network={
+#     ssid="network name"
+#    scan_ssid=1
+#    psk="password"
+#}
+#EOF
 
-sudo chmod 777 /etc/wpa_supplicant
+#sudo chmod 777 /etc/wpa_supplicant
 
-sudo sed -i -e"s/^country=.*/country=$country/" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed -i -e"s/^    ssid=.*/    ssid=\"$name\""/" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed -i -e"s/^    psk=.*/    psk=\"$pword/""/" /etc/wpa_supplicant/wpa_supplicant.conf
+#sudo sed -i -e"s/^country=.*/country=$country/" /etc/wpa_supplicant/wpa_supplicant.conf
+#sudo sed -i -e"s/^    ssid=.*/    ssid=\"$name\""/" /etc/wpa_supplicant/wpa_supplicant.conf
+#sudo sed -i -e"s/^    psk=.*/    psk=\"$pword/""/" /etc/wpa_supplicant/wpa_supplicant.conf
 
-sudo chmod 755 /etc/wpa_supplicant
+echo "" >> /etc/wpa_supplicant/wpa_supplicant.conf
+exho "country=$country" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo "network={" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo "	ssid=\"$SSID\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo "	psk=\"$PASS\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo "	key_mgmt=WPA-PSK" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo "}" >> /etc/wpa_supplicant/wpa_supplicant.conf
 
-sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf
+#sudo chmod 755 /etc/wpa_supplicant
+
+#sudo chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf
